d73a98df77c6d5aafd4ffb896e7f49de96b9b719
Aarón <aaron.sn.1988@gmail.com>
2018-10-05 11:34:41 +0200

    Update modal.js and modal.scss
    Added onclose animation for modal windows

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -7,7 +7,17 @@ function openModal(modalName){
 
 function closeModal(modalName) {
     shownModal = document.getElementById(modalName);
+    animationCloseModal(shownModal);
+}
+
+function animationCloseModal(elem) {    
+    elem.getElementsByClassName("modal-content")[0].className += ' close-modal-content';
+    setTimeout(hideModal,700);
+}
+
+function hideModal() {
     shownModal.style.display = "none";
+    shownModal.getElementsByClassName("modal-content")[0].classList.remove("close-modal-content");
 }
 
 function openImageModal(elem, modalName) {
@@ -21,6 +31,6 @@ function openImageModal(elem, modalName) {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == shownModal) {
-        shownModal.style.display = "none";
+        animationCloseModal(shownModal);
     }
 }
